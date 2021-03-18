5b4d811f10d7aa626d98b913c33d56052fb63aa8
Agustina Bustelo <agustinabustelo@MacBook-Air.local>
2021-03-17 22:07:04 -0300

    siendo exitosa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAIPractica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
